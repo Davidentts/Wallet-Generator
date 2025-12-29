fb9a327ad871142d11d0f3591af559f6df28a98b
Davidentt <Davidentt@yandex.ru>
2025-12-29 20:22:10 +0300

    Update logic

--- src/wallet_generator/tron/__main__.py
+++ src/wallet_generator/tron/__main__.py
@@ -1,23 +1,40 @@
+from datetime import datetime
+from pathlib import Path
+
 from wallet_generator.common.config import BASE_DIR
+from wallet_generator.common.encoding_tools.gpg import GPG
 from wallet_generator.common.schemas import (
     Wallet,
     WalletRegistry,
 )
 from wallet_generator.common.utils.input import (
     get_wallet_count,
-    ask_user,
+    ask_user_short,
     get_list_of_groups,
 )
 from wallet_generator.common.utils.output import (
     save_seed_to_file,
     save_wallets_to_file,
 )
-from .core import generate_seed_phrase, generate_wallets, save_wallets_to_keepass
+from .core import (
+    generate_seed_phrase,
+    generate_wallets,
+    save_wallets_to_keepass,
+    save_files_with_secrets,
+    parsing_input_parameters,
+)
 
 
 def main():
     try:
         print("Welcome to Tron wallet generator!")
+
+        save_dir = (
+            Path(BASE_DIR)
+            / f"tron_wallets_{datetime.now().strftime("%Y-%m-%d_%H-%M-%S")}"
+        )
+        save_dir.mkdir(parents=True, exist_ok=True)
+        encr_pass_file = parsing_input_parameters()
 
         wallet_count = get_wallet_count()
         registry = WalletRegistry()
@@ -29,40 +46,51 @@ def main():
             count=wallet_count,
         )
 
-        if ask_user("Would you like to distribute wallets into groups?"):
+        if ask_user_short("Would you like to distribute wallets into groups?"):
             get_list_of_groups(registry, maximum=wallet_count)
         registry.dist_wallets_into_groups(wallets)
 
-        if ask_user("Do you want to save wallets and seed to file without encryption?"):
-            save_wallets_to_file(
+        # gpg = GPG()
+        # if ask_user_short(
+        #     "Do you want to save wallets and seed to file without encryption?"
+        # ):
+        #     save_wallets_to_file(
+        #         registry=registry,
+        #         path=BASE_DIR,
+        #         filename="tron_wallets.txt",
+        #         add_private_key=True,
+        #     )
+        #     save_seed_to_file(
+        #         seed_phrase=registry.main_group.seed_phrase,
+        #         path=BASE_DIR,
+        #     )
+        #     save_wallets_to_file(
+        #         registry=registry,
+        #         path=BASE_DIR,
+        #     )
+        #     print(f"✅ Seed phrase saved to '{BASE_DIR}/seed_phrase.txt'")
+        #     print(f"✅ {wallet_count} wallets saved to '{BASE_DIR}/tron_wallets.txt'")
+        #     print(
+        #         f"✅ {wallet_count} wallets saved to '{BASE_DIR}/tron_wallets_no_private.txt'"
+        #     )
+        keepass_file: Path | None = None
+        if ask_user_short(question="Do you want to put wallets into keepass?"):
+            keepass_file = save_wallets_to_keepass(
                 registry=registry,
-                path=BASE_DIR,
-                filename="tron_wallets.txt",
-                add_private_key=True,
-            )
-            save_seed_to_file(
-                seed_phrase=registry.main_group.seed_phrase,
-                path=BASE_DIR,
-            )
-            save_wallets_to_file(
-                registry=registry,
-                path=BASE_DIR,
-            )
-            print(f"✅ Seed phrase saved to '{BASE_DIR}/seed_phrase.txt'")
-            print(f"✅ {wallet_count} wallets saved to '{BASE_DIR}/tron_wallets.txt'")
-            print(
-                f"✅ {wallet_count} wallets saved to '{BASE_DIR}/tron_wallets_no_private.txt'"
-            )
-
-        if ask_user(question="Do you want to put wallets into keepass?"):
-            save_wallets_to_keepass(
-                registry=registry,
-                path=BASE_DIR,
+                path=save_dir,
+                pass_file=encr_pass_file if encr_pass_file else None,
             )
             print("\n====================================")
             print("✅ KeePass database successfully created!")
-            print(f"→ Database file: {BASE_DIR}/wallets.kdbx")
+            print(f"→ Database file: {save_dir}/wallets.kdbx")
             print("====================================\n")
+
+        save_files_with_secrets(
+            registry,
+            encr_pass_file,
+            save_dir,
+            keepass_file=keepass_file if keepass_file is not None else None,
+        )
     except KeyboardInterrupt:
         print("\nProcess was interrupted!")
 
--- src/wallet_generator/tron/core.py
+++ src/wallet_generator/tron/core.py
@@ -1,3 +1,6 @@
+import argparse
+from dataclasses import dataclass
+import sys
 from pathlib import Path
 
 from mnemonic import Mnemonic
@@ -5,8 +8,9 @@ from tronpy.keys import PrivateKey
 from bip32utils import BIP32Key, BIP32_HARDEN
 
 from wallet_generator.common.encoding_tools import KeePass
+from wallet_generator.common.encoding_tools.gpg import GPG
 from wallet_generator.common.schemas import Wallet, WalletRegistry
-from wallet_generator.common.utils.input import get_password
+from wallet_generator.common.utils.input import get_password, ask_user_short, ask_user
 
 
 def generate_seed_phrase(strength: int = 256) -> str:
@@ -40,13 +44,120 @@ def generate_private_key_from_seed(seed: bytes, index: int) -> PrivateKey:
     return PrivateKey(key.PrivateKey())
 
 
+def parsing_input_parameters() -> Path:
+    parser = argparse.ArgumentParser(
+        description="Processing the input file with password."
+    )
+    parser.add_argument(
+        "--encrypted-password",
+        "-p",
+        type=str,
+        help="Path to file with encrypted password.",
+    )
+
+    args = parser.parse_args()
+    filepath = Path(args.encrypted_password).resolve()
+
+    if not filepath.is_file():
+        print(f"Error: file '{filepath}' not found", file=sys.stderr)
+        sys.exit(1)
+
+    return filepath
+
+
+def save_files_with_secrets(
+    registry: WalletRegistry,
+    pass_file: Path,
+    save_dir: Path,
+    keepass_file: Path,
+) -> None:
+
+    @dataclass
+    class FileParam:
+        name: str
+        filename: str
+        data: str | None = None
+        path_file: Path | None = None
+
+    files_params = [
+        FileParam(
+            name="seed phrase",
+            filename="seed_phrase",
+            data=registry.main_group.seed_phrase,
+        ),
+        FileParam(
+            name="wallets with private keys",
+            filename="tron_wallets_SECRET",
+            data=registry.to_str(verbose=True),
+        ),
+        FileParam(
+            name="list of wallets", filename="tron_wallets", data=registry.to_str()
+        ),
+        FileParam(
+            name="KeePass file", filename=keepass_file.name, path_file=keepass_file
+        ),
+    ]
+    verbose = [
+        "Save without encryption (not recommended)",
+        "Encrypt using symmetric encryption",
+        "Encrypt using asymmetric encryption",
+        "Do not save",
+    ]
+    gpg = GPG()
+    for params in files_params:
+        result = ask_user(
+            f"How do you want to save {params.name}?",
+            verbose,
+            func_for_print=lambda x: x,
+            func_for_result=lambda x: x,
+        )
+        file_data: str | None = None
+        match result:
+            case "Save without encryption (not recommended)":
+                file_data = params.data
+                params.filename = params.filename + ".txt"
+            case "Encrypt using symmetric encryption":
+                if params.data is not None:
+                    file_data = gpg.encrypt_message_symmetric(
+                        message=params.data, password=gpg.decrypt_from_file(pass_file)
+                    )
+                    params.filename = params.filename + ".sym"
+                elif params.path_file is not None:
+                    gpg.encrypt_file_symmetric(
+                        file=params.path_file, password=gpg.decrypt_from_file(pass_file)
+                    )
+            case "Encrypt using asymmetric encryption":
+                public_key = ask_user(
+                    f"What key would you like to use?",
+                    gpg.public_keys,
+                    func_for_print=lambda x: f"UID: {x.uids[0].name}, {x.uids[0].email} 0x{x.keyid} {x.created}",
+                    func_for_result=lambda x: x,
+                )
+                if params.data is not None:
+                    file_data = gpg.encrypt_message(
+                        message=params.data, recipient=public_key
+                    )
+                    params.filename = params.filename + ".asc"
+                elif params.path_file is not None:
+                    gpg.encrypt_file(file=params.path_file, recipient=public_key)
+        if file_data:
+            file = save_dir / params.filename
+            file.write_text(file_data)
+
+
 def save_wallets_to_keepass(
     registry: WalletRegistry,
     path: Path,
     filename: str = "wallets.kdbx",
-) -> None:
+    pass_file: Path | None = None,
+) -> Path:
     """Save generated Tron wallets to a KeePass storage file."""
-    password = get_password()
+    if pass_file:
+        gpg = GPG()
+        password = gpg.decrypt_from_file(pass_file)
+    else:
+        password = get_password()
+
     keepass = KeePass(
         password=password,
         registry=registry,
@@ -56,3 +167,5 @@ def save_wallets_to_keepass(
     keepass.add_seed_phrase(registry.main_group.seed_phrase)
     for group in registry:
         keepass.fill_group_with_wallets(group)
+
+    return path / filename
